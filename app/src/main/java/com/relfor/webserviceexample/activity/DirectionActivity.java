2f95079caa10504a97922db5724f2c99ccd595b6
Rizwanul Haque <rizwan@relfor.com>
2018-06-24 12:52:31 +0530

    Added final project

--- app/src/main/java/com/relfor/webserviceexample/activity/DirectionActivity.java
+++ app/src/main/java/com/relfor/webserviceexample/activity/DirectionActivity.java
@@ -6,10 +6,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.relfor.webserviceexample.Constant;
 import com.relfor.webserviceexample.R;
+import com.relfor.webserviceexample.data.direction.DirectionResponse;
+import com.relfor.webserviceexample.data.direction.Element;
+import com.relfor.webserviceexample.service.ApiClient;
+import com.relfor.webserviceexample.service.MoviesEndpoint;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static com.relfor.webserviceexample.Constant.ALTERNATIVE_PLACE_HOLDER;
 import static com.relfor.webserviceexample.Constant.BASE_URL_ETA;
@@ -22,6 +31,7 @@ import static com.relfor.webserviceexample.Constant.ORIGIN_PLACE_HOLDER;
 public class DirectionActivity extends AppCompatActivity {
     TextView txtDistance, txtTime;
     EditText edtSource, edtDestination;
+    ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +42,11 @@ public class DirectionActivity extends AppCompatActivity {
         edtDestination = findViewById(R.id.edtDestination);
         txtDistance = findViewById(R.id.txtDistance);
         txtTime = findViewById(R.id.txtTime);
+        progress = findViewById(R.id.progress);
     }
 
     public void onSearchClick(View view) {
-
+        progress.setVisibility(View.VISIBLE);
         String origin = edtSource.getText().toString().trim();
         String destination = edtDestination.getText().toString().trim();
 
@@ -43,8 +54,32 @@ public class DirectionActivity extends AppCompatActivity {
                 destination + MODE_PLACE_HOLDER + "driving" + ALTERNATIVE_PLACE_HOLDER + false
                 + KEY_PLACEHOLDER + DIRECTION_API_KEY;
 
-        Log.e("API URL", "URL "+url);
+        MoviesEndpoint apiService = ApiClient.getClient().create(MoviesEndpoint.class);
 
+        Call<DirectionResponse> callDirection = apiService.getDirectionData(url);
+
+        callDirection.enqueue(new Callback<DirectionResponse>() {
+            @Override
+            public void onResponse(Call<DirectionResponse> call, Response<DirectionResponse> response) {
+                progress.setVisibility(View.GONE);
+                DirectionResponse directionResponse = response.body();
+                Element element = directionResponse.getRows().get(0).getElements().get(0);
+
+                String distance = element.getDistance().getText();
+                String time = element.getDuration().getText();
+
+                txtDistance.setText("Distance in km : " + distance);
+                txtTime.setText("Time in mins : " + time);
+
+            }
+
+            @Override
+            public void onFailure(Call<DirectionResponse> call, Throwable t) {
+                progress.setVisibility(View.GONE);
+            }
+        });
+
+        Log.e("API URL", "URL " + url);
 
 
     }
