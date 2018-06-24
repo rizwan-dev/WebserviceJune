069cc2ee58f8fa49ff7555500a606382e3d74446
Rizwanul Haque <rizwan@relfor.com>
2018-06-24 12:30:35 +0530

    Added constants

--- app/src/main/java/com/relfor/webserviceexample/Constant.java
+++ app/src/main/java/com/relfor/webserviceexample/Constant.java
@@ -2,8 +2,13 @@ package com.relfor.webserviceexample;
 
 public class Constant {
 
-    public static final String BASE_URL_ETA = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=";
+    public static final String BASE_URL_ETA = "https://maps.googleapis.com/maps/api/distancematrix/json";
     public static final String DIRECTION_API_KEY = "AIzaSyCEq9sfQp6-nvD95D8mKj4ffTZv74s92Cw";
+    public static final String ORIGIN_PLACE_HOLDER = "?origins=";
+    public static final String DESTINATION_PLACE_HOLDER = "&destinations=";
+    public static final String MODE_PLACE_HOLDER = "&mode=";
+    public static final String ALTERNATIVE_PLACE_HOLDER = "&alternatives=";
+    public static final String KEY_PLACEHOLDER = "&key=";
 
 //    https://maps.googleapis.com/maps/api/distancematrix/json?origins=18.541503,%2073.895366&destinations=18.540638,%2073.892448&mode=walking&alternatives=false&key=AIzaSyCEq9sfQp6-nvD95D8mKj4ffTZv74s92Cw
 }
--- app/src/main/java/com/relfor/webserviceexample/activity/DirectionActivity.java
+++ app/src/main/java/com/relfor/webserviceexample/activity/DirectionActivity.java
@@ -2,12 +2,22 @@ package com.relfor.webserviceexample.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.relfor.webserviceexample.Constant;
 import com.relfor.webserviceexample.R;
+
+import static com.relfor.webserviceexample.Constant.ALTERNATIVE_PLACE_HOLDER;
+import static com.relfor.webserviceexample.Constant.BASE_URL_ETA;
+import static com.relfor.webserviceexample.Constant.DESTINATION_PLACE_HOLDER;
+import static com.relfor.webserviceexample.Constant.DIRECTION_API_KEY;
+import static com.relfor.webserviceexample.Constant.KEY_PLACEHOLDER;
+import static com.relfor.webserviceexample.Constant.MODE_PLACE_HOLDER;
+import static com.relfor.webserviceexample.Constant.ORIGIN_PLACE_HOLDER;
 
 public class DirectionActivity extends AppCompatActivity {
     TextView txtDistance, txtTime;
@@ -24,7 +34,18 @@ public class DirectionActivity extends AppCompatActivity {
         txtTime = findViewById(R.id.txtTime);
     }
 
-    public void onSearchClick(View view){
+    public void onSearchClick(View view) {
+
+        String origin = edtSource.getText().toString().trim();
+        String destination = edtDestination.getText().toString().trim();
+
+        String url = BASE_URL_ETA + ORIGIN_PLACE_HOLDER + origin + DESTINATION_PLACE_HOLDER +
+                destination + MODE_PLACE_HOLDER + "driving" + ALTERNATIVE_PLACE_HOLDER + false
+                + KEY_PLACEHOLDER + DIRECTION_API_KEY;
+
+        Log.e("API URL", "URL "+url);
+
+
 
     }
 }
